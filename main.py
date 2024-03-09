cd48d947cbc71a2c59cabcf4290babfa624b381d
Archit Sood <architsood99@hotmail.com>
2024-03-09 15:06:34 +0530

    new adds.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-from datetime import datetime
-
+import uuid
+from datetime import datetime, timedelta
+import random
 from confluent_kafka import SerializingProducer
 import simplejson as json
 
@@ -28,24 +29,94 @@ TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
 WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
 EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
 
+random.seed(1)
 start_time = datetime.now()
 start_location = LONDON_COORDINATES.copy()
 
 
 def simulate_vehicle_movement():
     global start_location
+    start_location['latitude'] += LATITUDE_INCREMENT
+    start_location['longitude'] += LONGITUDE_INCREMENT
+
+    start_location['latitude'] += random.uniform(-0.0005, 0.0005)
+    start_location['longitude'] += random.uniform(-0.0005, 0.0005)
+
+    return start_location
+
+
+def get_next_time():
+    global start_time
+    start_time += timedelta(seconds=random.randint(30, 60))
+    return start_time
+
+
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'location': location,
+        'timestamp': timestamp,
+        'temperature': random.uniform(-5, 26),
+        'weathercondition': random.choice(['Sunny', 'Rainy', 'Cloudy', 'Snowy']),
+        'precipitation': random.uniform(0, 25),
+        'windspeed': random.uniform(0, 30),  # km/h
+        'humidity': random.uniform(0, 100),  # percentage
+        'airqualityIndex': random.uniform(0, 500)  # AQI value goes here
+
+    }
+
+
+def generate_gps_data(device_id, timestamp, vehicle_type='private'):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'timestamp': timestamp,
+        'speed': random.uniform(0, 40),  # km/h
+        'direction': 'North-East',
+        'vehicle_type': vehicle_type
+
+    }
+
+
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'cameraId': camera_id,
+        'location': location,
+        'timestamp': timestamp,
+        'snapshot': 'Base64EncodedImage'
+    }
 
 
 def generate_vehicle_data(device_id):
     location = simulate_vehicle_movement()
+    return {
+        'id': str(uuid.uuid4()),
+        'deviceId': device_id,
+        'timestamp': get_next_time().isoformat(),
+        'location': (location['latitude'], location['longitude']),
+        'speed': random.uniform(10, 20),
+        'direction': 'North-East',
+        'make': 'BMW',
+        'model': 'X5',
+        'year': 2019,
+        'fueltype': 'hybrid'
+    }
 
 
 def simulate_journey(producer, device_id):
     while True:
         vehicle_data = generate_vehicle_data(device_id)
+        gps_data = generate_gps_data(device_id, vehicle_data['timestamp'])
+        traffic_camera_data = generate_traffic_camera_data(device_id,
+                                                           vehicle_data['timestramp'], vehicle_data['location'],
+                                                           camera_id='Camera123')
+        weather_data = generate_weather_data(device_id, vehicle_data['timestamp'], vehicle_data['location'])
 
 
-if __name__ == "_main__":
+if __name__ == "__main__":
     producer_config = {
         'bootstrap.servers': KAFKA_BOOTSTRAP_SERVER,
         'error_cb': lambda err: print(f'Kafka error: {err}')
@@ -56,6 +127,6 @@ if __name__ == "_main__":
         simulate_journey(producer, 'Vehicle-archit')
 
     except KeyboardInterrupt:
-        print('Simulate ended by the user')
+        print('Simulation ended by the user')
     except Exception as e:
         print(f'Unexpected error: {e}')
